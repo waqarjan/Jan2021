eb5da07d2f56cecac0b36dca9c0097f35679f70e
AJan <wakarjan@hotmail.com>
2021-08-17 20:49:51 -0400

    Flash is on fire

--- src/main/java/Flash.java
+++ src/main/java/Flash.java
@@ -2,5 +2,6 @@ public class Flash {
     public static void main(String[] args) {
         System.out.println("Flash runs really fast");
         //Flash shines
+        //Flash is on fire
     }
 }
